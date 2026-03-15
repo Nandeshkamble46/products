012f5f31be6e1f2bfe84d4252898f87fcf5ab78d
user.name like:Nandesh Kamble <user.email like:nandeshkamble9743@gmail.com>
2026-03-15 14:37:51 +0530

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <nav className="bg-black text-white p-4 flex justify-between">
+            <Link to="/" className="text-xl font-bold">
+                Products
+            </Link>
+
+            <div className="space-x-4">
+                <Link to="/">Products</Link>
+                <Link to="/cart">Cart</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
